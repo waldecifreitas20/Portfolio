e2e524005a1cb68a7442286abbaa78d5ed6ad52f
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-07-26 14:53:39 -0300

    feat: navbar for mobile devices

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@ function handleMenuBehavior() {
   const asideMenu = document.getElementById("top-navbar");
   const btnOpenMenu = document.getElementById("btn-open-menu");
   const btnCloseMenu = document.getElementById("btn-close-menu");
+ 
+  const body = document.getElementsByTagName("body")[0];
 
+  
+  
   function openMenu() {
     asideMenu.className = "open-menu";
+    body.className = "disable-scroll";
   }
-
+  
   function closeMenu() {
     asideMenu.className = "close-menu";
+    body.className = "enable-scroll";
   }
 
   btnOpenMenu.addEventListener("click", openMenu);
