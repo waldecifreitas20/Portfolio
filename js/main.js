7d021e1b1fabc1becbfb778c4c786327e2279eb3
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-07-26 15:17:50 -0300

    feat: font size for accessibility

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+let fontSize = 1;
+
+function main() {
+  handleMenuBehavior();
+  handleFontSizeControl();
+}
+
+
 function handleMenuBehavior() {
   const asideMenu = document.getElementById("top-navbar");
   const btnOpenMenu = document.getElementById("btn-open-menu");
   const btnCloseMenu = document.getElementById("btn-close-menu");
- 
   const body = document.getElementsByTagName("body")[0];
 
-  
-  
   function openMenu() {
     asideMenu.className = "open-menu";
     body.className = "disable-scroll";
   }
-  
+
   function closeMenu() {
     asideMenu.className = "close-menu";
     body.className = "enable-scroll";
@@ -22,4 +27,29 @@ function handleMenuBehavior() {
 
 }
 
-handleMenuBehavior();
+
+function handleFontSizeControl() {
+  const btnFontBigger = document.getElementById("btn-font-bigger");
+  const btnFontSmaller = document.getElementById("btn-font-smaller");
+  const body = document.getElementsByTagName("body")[0];
+
+  function increaseFontSize() {
+    if (fontSize < 1.8) {
+      fontSize += 0.2;
+      body.style.fontSize = `${fontSize}rem`;
+    }
+  }
+
+  function decreaseFontSize() {
+    if (fontSize > 0.8) {
+      fontSize -= 0.2;
+      body.style.fontSize = `${fontSize}rem`;
+    }
+  }
+
+  btnFontBigger.addEventListener("click", increaseFontSize);
+  btnFontSmaller.addEventListener("click", decreaseFontSize);
+
+}
+
+main();
